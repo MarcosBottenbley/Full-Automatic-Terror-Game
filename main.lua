17b064cadad78c0d545df9db1a01e09ea3666c74
David <dmill118@jhu.edu>
2015-09-14 19:23:50 -0400

    Implemented green gel and toast sprites, added sound effect (made using SFXR)

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 time = 0
 
+
+
 local Enemy = {
 	x = 10, y = 10,
 	vx = 20, vy = 20,
@@ -24,25 +26,32 @@ function Enemy.new(x, y, v)
 end
 
 function Enemy:draw()
-	love.graphics.circle("fill", self.x, self.y, self.r, 100)
+	--love.graphics.circle("fill", self.x, self.y, self.r, 100)
+	love.graphics.draw(
+		enemy_sprite, 
+		self.x, self.y, 
+		0, 
+		.5, .5, 
+		enemy_width/2, enemy_width/2
+	)
 end
 
 function Enemy:update(dt)
 	self.x = self.x + self.vx*dt
 	self.y = self.y + self.vy*dt
 
-	if self.x < self.r or self.x > width-self.r then
+	if self.x < enemy_width/4 or self.x > width-enemy_width/4 then
 		self:bounce(1)
 	end
 
-	if self.y < self.r or self.y > height-self.r then
+	if self.y < enemy_height/4 or self.y > height-enemy_height/4 then
 		self:bounce(0)
 	end
 end
 
 function Enemy:direction()
-	if math.random() == 1 then
-		self:bounce(math.random())
+	if math.floor(math.random(0,2)) == 1 then
+		self:bounce(1)
 	end
 end
 
@@ -65,15 +74,28 @@ end
 function love.load(arg)
 	width = love.window.getWidth()
 	height = love.window.getHeight()
+	
+	enemy_sprite = love.graphics.newImage("gfx/gel_smaller.png")
+	enemy_width = enemy_sprite:getWidth()
+	enemy_height = enemy_sprite:getHeight()
+	
+	player_sprite = love.graphics.newImage("gfx/toast.jpg")
+	player_width = player_sprite:getWidth()
+	player_height = player_sprite:getHeight()
+	
+	blip = love.audio.newSource("sfx/bump.ogg")
+	blip:setLooping(false)
 
 	enemies = {}
 
 	for i = 1, 10 do
-		table.insert(enemies, Enemy(math.random(800), math.random(600), math.random(20,60)))
+		table.insert(enemies, Enemy(math.random(800 - enemy_width/2)
+		, math.random(600 - enemy_height/2), math.random(20,60)))
 	end
 
-	player1 = {x = width/2, y = height/2, width = 50, height = 50, speed = 20}
-	color = {r = 26, g = 237, b = 19}
+	player1 = {x = width/2, y = height/2, 
+	width = player_width/2, height = player_height/2, speed = 20}
+	--color = {r = 26, g = 237, b = 19}
 end
 
 function love.update(dt)
@@ -86,45 +108,57 @@ function love.update(dt)
 	if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
 		player1.x = player1.x-player1.speed
 
-		if player1.x < 1 then
-			player1.x = 1
+		if player1.x < (1 + player1.width/4) then
+			player1.x = (1 + player1.width/4)
+			blip:play()
 		end
 	end
 
 	if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
 		player1.x = player1.x+player1.speed
 
-		if player1.x > width-player1.width+1 then
-			player1.x = width-player1.width+1
+		if player1.x > width-player1.width/4+1 then
+			player1.x = width-player1.width/4+1
+			blip:play()
 		end
 	end
 
 	if love.keyboard.isDown('down') or love.keyboard.isDown('s') then
 		player1.y = player1.y + player1.speed
 
-		if player1.y > height-player1.height+1 then
-			player1.y = height-player1.height+1
+		if player1.y > height-player1.height/4+1 then
+			player1.y = height-player1.height/4+1
+			blip:play()
 		end
 	end
 
 	if love.keyboard.isDown('up') or love.keyboard.isDown('w') then
 		player1.y = player1.y-player1.speed
 
-		if player1.y < 1 then
-			player1.y = 1
+		if player1.y < (1 + player1.height/4) then
+			player1.y = (1 + player1.height/4)
+			blip:play()
 		end
 	end
 end
 
 function love.draw(dt)
-	love.graphics.setColor(color.r,color.g,color.b,255)
-	love.graphics.rectangle("fill", player1.x , player1.y , player1.width , player1.height)
+	--love.graphics.setColor(color.r,color.g,color.b,255)
+	--love.graphics.rectangle("fill", player1.x , player1.y , player1.width , player1.height)
 
+	love.graphics.draw(
+		player_sprite, 
+		player1.x, player1.y, 
+		0, 
+		.5, .5, 
+		player_width/2, player_width/2
+	)
+	
 	for _, e in ipairs(enemies) do
 		e:draw()
 	end
 
-	love.graphics.setColor(255, 255, 255, 255)
+	--love.graphics.setColor(255, 255, 255, 255)
 end
 
 function love.keypressed(key)
