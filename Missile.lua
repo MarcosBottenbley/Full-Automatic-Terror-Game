0366acb7e8c570f80a1d451ab9787578435a2b80
David <dmill118@jhu.edu>
2015-10-17 18:53:00 -0400

    Made some more changes to missile, its turning speed and distance at which it locks on can be set now

--- Missile.lua
+++ Missile.lua
@@ -14,8 +14,8 @@ Object = require("Object")
 math.randomseed(os.time())
 
 local Missile = {
-	vel = 350, turnSpeed = math.pi*(3/2),
-	lockDistance, lockTime,
+	vel = 350, turnSpeed = math.pi*2,
+	lockDistance = 300, lockTime,
 	id = 3, collided = false,
 	width = 10, height = 10,
 	bounding_rad = 12.5, angle = 0,
@@ -46,24 +46,39 @@ function Missile:_init(x, y, v, a)
 		self.states,
 		self.delay)
 
-	self.hb_1 = {self.x, self.y, self.bounding_rad}-- + self.width/2, self.y + 5, self.bounding_rad}
+	self.hb_1 = {self.x, self.y, self.bounding_rad}
+	self.target = nil
 end
 
 --- Missile, hitbox speed and direction
 
 function Missile:update(dt)
-	local closestDist = 600
+	local closestDist = self.lockDistance
+	
 	self.target_x = nil
 	self.target_y = nil
-	for _, o in ipairs(objects) do
-		if self:dist(o) < closestDist and o:getID() == 1 then
-			self.target_x = o:getX()
-			self.target_y = o:getY()
-			self.target = o
-			closestDist = self:dist(o)
+	
+	if self.target == nil then
+		for _, o in ipairs(objects) do
+			if self:dist(o) < closestDist and o:getID() == 1 then
+				self.target_x = o:getX()
+				self.target_y = o:getY()
+				self.target = o
+				closestDist = self:dist(o)
+			end
+		end
+	else
+		self.target_x = self.target:getX()
+		self.target_y = self.target:getY()
+		if self.target.collided then
+			self.target = nil
 		end
 	end
 	
+	--Code for calculating target angle. It's kind of messy right now,
+	--but it gets the angle (0 to 2*pi) between the missile and the target.
+	--If the missile is not targeting any object, then the target angle 
+	--becomes the current angle and the trajectory won't change.
 	local target_angle = 0
 	if self.target_x ~= nil and self.target_y ~= nil then
 		if self.target_x - self.x > 0 then
@@ -78,6 +93,9 @@ function Missile:update(dt)
 		target_angle = self.angle
 	end
 	
+	--Code for changing the angle and moving the missile. angleDir will
+	--return whether the missile should rotate clockwise, counterclockwise
+	--or not at all, and the speed of rotation is determined by turnSpeed.
 	local rotate = self:angleDir(self.angle, target_angle)
 	local angvel = rotate * self.turnSpeed * dt
 	self.angle = self.angle + angvel
@@ -91,13 +109,7 @@ end
 function Missile:draw()
 	love.graphics.setColor(255,188,188,188)
 	
-	local vertices = {
-	self.x - 12.5, self.y - 10.825,
-	self.x + 12.5, self.y - 10.825,
-	self.x, self.y + 10.825
-	}
-	
-	love.graphics.polygon('fill', vertices)
+	love.graphics.circle('fill', self.x, self.y, self.bounding_rad - 1, 100)
 	
 	love.graphics.setColor(255,255,255,255)
 end
@@ -112,10 +124,12 @@ function Missile:exited_map(width, height)
 	end
 end
 
+--Returns distance from missile to object
 function Missile:dist(obj)
 	return math.sqrt((obj:getX() - self.x)^2 + (obj:getY() - self.y)^2)
 end
 
+--Calculates direction to turn based on current angle and angle to target
 function Missile:angleDir(a_start, a_finish)
 	local diff = a_start - a_finish
 	
