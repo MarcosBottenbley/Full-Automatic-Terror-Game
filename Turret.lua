9d6089087770accb724fc3ad6983f4eb018d5f0b
David <dmill118@jhu.edu>
2015-11-10 20:32:25 -0500

    Turret aims at player and shoots two bullets at constant rate

--- Turret.lua
+++ Turret.lua
@@ -18,7 +18,7 @@ local Turret = {
 	frames = 2, states = 2,
 	delay = 0.12, sprites = {},
 	bounding_rad = 25, type = 't',
-	angle
+	angle, shooting, bullet_timer = 0
 }
 Turret.__index = Turret
 
@@ -38,17 +38,31 @@ end
 
 function Turret:update(dt, swidth, sheight, px, py)
 	Enemy.update(self, dt, swidth, sheight)
+	self.bullet_timer = self.bullet_timer + dt
 
 	self.angle = math.atan((self.y - py) / (px - self.x))
-	if px > self.x then
+	if px < self.x then
 		self.angle = self.angle + math.pi
 	end
-
+	
+	if self.bullet_timer > .5 then
+		self:shoot()
+		self.bullet_timer = 0
+	end
 
 end
 
 function Turret:draw()
-	Object.draw(self, 255, 255, 255, math.pi*(3/2) - self.angle)
+	Object.draw(self, 255, 255, 255, math.pi/2 - self.angle)
+end
+
+function Turret:shoot()
+	local startposx = self.x + 18.5 * math.cos(self.angle)
+	local startposy = self.y - 18.5 * math.sin(self.angle)
+	local b1 = EnemyBullet(startposx + 10*math.sin(self.angle), startposy + 10*math.cos(self.angle), 600, self.angle) --magic numbers errywhere
+	local b2 = EnemyBullet(startposx - 10*math.sin(self.angle), startposy - 10*math.cos(self.angle), 600, self.angle) --magic numbers errywhere
+	table.insert(objects, b1)
+	table.insert(objects, b2)
 end
 
 function Turret:getType()
